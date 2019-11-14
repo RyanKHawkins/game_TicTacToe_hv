284c76b7a55e3fa05f01f726393155680ebd56cc
Ryan Hawkins <rkhawkins80@hotmail.com>
2019-11-14 16:32:02 +0000

    added vertical win

--- main.py
+++ main.py
@@ -29,25 +29,29 @@ def display_board(board):
 
 def play_game():
     num_players = input("How many players? (0 - 2):  ")
-    
+
 
 def player_move(player):
     pass
 
 
-def computer_move():
-    pass
+def computer_move(board):
+    possible_moves = []
+    for space in board:
+        if space_free(space):
+            possible_moves.append(space)
+    return possible_moves
 
 
 def space_free(space):
     return space not in ["X", "O"]
 
 
-def random_move():
+def random_computer_move():
     pass
 
 
-def smart_move():
+def smart_computer_move():
     pass
 
 
@@ -55,12 +59,15 @@ def game_won():
     pass
 
 
-def vertical_win():
-    pass
-
-
 def horizontal_win():
     pass
+
+
+def vertical_win(player):
+    for column in range(3):
+        if board[column] == board[column + 3] == board[column + 6] == player:
+            return True
+    return False
 
 
 def diagonal_win():
