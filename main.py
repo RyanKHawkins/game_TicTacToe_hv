fa95b2e917546eedb29ff9d3c74d80f0ddd19c81
Ryan Hawkins <rkhawkins80@hotmail.com>
2019-11-14 15:38:16 +0000

    set function order

--- main.py
+++ main.py
@@ -4,6 +4,62 @@
 import random
 import os
 
+
 def clear():
     os.system('cls' if os.name == 'nt' else 'clear')
 
+
+def create_board():
+    pass
+
+
+def display_board():
+    pass
+
+
+def play_game():
+    pass
+
+
+def player_move(player):
+    pass
+
+
+def computer_move():
+    pass
+
+
+def random_move():
+    pass
+
+
+def smart_move():
+    pass
+
+
+def game_won():
+    pass
+
+
+def vertical_win():
+    pass
+
+
+def horizontal_win():
+    pass
+
+
+def diagonal_win():
+    pass
+
+
+def board_full():
+    pass
+
+
+def game_tie():
+    pass
+
+
+def switch_players():
+    pass
